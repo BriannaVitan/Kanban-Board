bae268c58b73b110b5ddb034865a7214215e5507
BriannaVitan <briannavitan@gmail.com>
2024-11-21 07:50:11 -0600

    made changes to auth-routes and updated the login and token

---
+++ Develop/server/src/routes/auth-routes.ts
@@ -0,0 +1,32 @@
+import { Router, Request, Response } from 'express';
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+export const login = async (req: Request, res: Response) => {
+  // TODO: If the user exists and the password is correct, return a JWT token
+  // Utility function for generating a JWT
+  const {username, password} = req.body
+  const secretKey = process.env.JWT_SECRET_KEY;
+  if (!secretKey) {
+    throw new Error('Secret key is missing');
+  }
+  const user = await User.findOne({ where: { username } });
+
+  // If user does not exist, return null
+  if (!user) return res.status(401).json({message: "username not found"});
+
+  // Check if the provided password matches the stored hashed password
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) return res.status(401).json({message: "password not found"});;
+  // Generate a JWT token with an expiration of 1 hour
+const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+  return res.json({token})
+};
+
+const router = Router();
+
+// POST /login - Login a user
+router.post('/login', login);
+
+export default router;
