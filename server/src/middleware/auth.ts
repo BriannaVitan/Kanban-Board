d5986c66179fa433c14185bc7fce391205a1cff1
BriannaVitan <briannavitan@gmail.com>
2024-11-21 14:17:01 -0600

    got rid of the Develop folder

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,8 +7,7 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
-
-    // authorization header from the request
+  // authorization header from the request
     const authHeader = req.headers.authorization;
 
     // checking to see if authorization header is present
